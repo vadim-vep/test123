e1929a27b4e00eda96c0aefd8f9b133abc37b4e8
vadim-vep <vadim.vep@gmail.com>
2024-01-18 16:26:41 +0100

    test

--- test2/main.go
+++ test2/main.go
@@ -343,22 +343,4 @@ func calSettable[T any, S settable[T]]() T {
 	return v
 }
 
-func main() {
-	s := calSettable[String]()
-	//   ^^^^^^^^^^^^^^^^^^^^^
-	//   Here it shows "Cannot use *T as the type settable[T]"
-	println(s)
-}
-package main
-
-type String string
-
-func (s *String) Set(key string) {
-	*s = String(key)
-}
-
-type settable[T any] interface {
-	*T
-	Set(string)
-}
 
